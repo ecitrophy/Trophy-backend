0ebd73da7f26b8fbc91441a270c0d59633e11ff4
JuanNicolasGomez <nicolas.gm243@gmail.com>
2019-03-25 19:08:03 -0500

    First commit

---
+++ src/main/java/edu/eci/trophy/trophybackend/TrophyBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.trophy.trophybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrophyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrophyBackendApplication.class, args);
+	}
+
+}
